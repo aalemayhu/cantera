d1fee6285394a324e9d9f8b307a6ae167a543160
Alexander Alemayhu <alexander@alemayhu.com>
2018-11-14 08:40:32 +0100

    Create a Xcode project
    Closes: #2 (Setup Xcode project)

---
+++ cantera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cantera
+//
+//  Created by Alexander Alemayhu on 14/11/2018.
+//  Copyright © 2018 Alexander Alemayhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
