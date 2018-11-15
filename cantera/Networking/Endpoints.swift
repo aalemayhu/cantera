a535d872a77fa9f51e31fd948502d2d2ef21d25e
Alexander Alemayhu <alexander@alemayhu.com>
2018-11-15 03:14:03 +0100

    Inline baseImageURL
    No one else is using it.

--- cantera/Networking/Endpoints.swift
+++ cantera/Networking/Endpoints.swift
@@ -8,8 +8,6 @@
 
 import Foundation
 
-private let baseImageURL = URL(string: "https://images.finncdn.no/dynamic/480x360c/")
-
 enum Endpoints {
     case json
     case image(String)
@@ -19,7 +17,7 @@ enum Endpoints {
         case .json:
             return URL(string: "https://gist.githubusercontent.com/3lvis/3799feea005ed49942dcb56386ecec2b/raw/63249144485884d279d55f4f3907e37098f55c74/discover.json")
         case .image(let id):
-            return baseImageURL?.appendingPathComponent(id)
+            return URL(string: "https://images.finncdn.no/dynamic/480x360c/\(id)")
         }
     }
 }
